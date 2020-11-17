26b17d09c3bafcf07a91409bec426fae5a30c538
asep10001 <asep10001@mail.unpad.ac.id>
2020-11-17 11:25:03 +0700

    merubah input age dan input birthdate agar tidak crash ketika tidak di isi
    merubah input weight dan height di BMI calculator agar tidak crash ketika tidak di isi

--- app/src/main/java/id/kotlin/tugasminggu02/BmiCalculator.kt
+++ app/src/main/java/id/kotlin/tugasminggu02/BmiCalculator.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Window
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_bmi_calculator.*
 import kotlinx.android.synthetic.main.bmi_normal.*
@@ -17,52 +18,66 @@ class BmiCalculator : AppCompatActivity() {
         setContentView(R.layout.activity_bmi_calculator)
 
         buttonCalculateBmi.setOnClickListener {
+            val weight = this.findViewById<EditText>(R.id.inputWeightBmi).text
+            val height = this.findViewById<EditText>(R.id.inputHeightBmi).text
+            if (!weight.isNullOrEmpty() && !height.isNullOrEmpty()) {
 
 //            print(textResult.text)
-            Dialog(this).apply {
-                requestWindowFeature(Window.FEATURE_NO_TITLE)
-                setCancelable(true)
-                setContentView(R.layout.bmi_normal)
-                val weight = this@BmiCalculator.findViewById<EditText>(R.id.inputWeightBmi)
-                val height = this@BmiCalculator.findViewById<EditText>(R.id.inputHeightBmi)
-                val textResult = this.findViewById<TextView>(R.id.textResultBmi)
+                Dialog(this).apply {
+                    requestWindowFeature(Window.FEATURE_NO_TITLE)
+                    setCancelable(true)
+                    setContentView(R.layout.bmi_normal)
+                    val textResult = this.findViewById<TextView>(R.id.textResultBmi)
 
 //                val colorResult = this.findViewById(R.id.result) as LinearLayout
 
-                val hasil =
-                    (weight.text).toString().toDouble() / (height.text.toString().toDouble() / 100)
-                        .pow(2)
+                    val hasil =
+                        (weight).toString().toDouble() / (height.toString()
+                            .toDouble() / 100)
+                            .pow(2)
 
-                if (hasil < 16.0) {
-                    textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Severe Thinnes)"
-                    textResult.setBackgroundResource(R.color.severe_thinnes)
-                } else if (hasil in 17.0..16.0) {
-                    textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Moderate Thinnes)"
-                    textResult.setBackgroundResource(R.color.moderate_thinnes)
-                } else if (hasil in 17.0..19.0) {
-                    textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Mild Thinnes)"
-                    textResult.setBackgroundResource(R.color.mild_thinness)
-                } else if (hasil in 19.0..25.0) {
-                    textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Normal)"
-                    textResult.setBackgroundResource(R.color.normal)
-                } else if (hasil in 25.0..30.0) {
-                    textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Overweight)"
-                    textResult.setBackgroundResource(R.color.overweight)
-                } else if (hasil in 30.0..35.0) {
-                    textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Obesity Class 1)"
-                    textResult.setBackgroundResource(R.color.obesity1)
-                } else if (hasil in 35.0..40.0) {
-                    textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Obesity Class 2)"
-                    textResult.setBackgroundResource(R.color.obesity2)
-                } else if (hasil > 40.0) {
-                    textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Obesity Class 3)"
-                    textResult.setBackgroundResource(R.color.obesity3)
-                }
+                    when {
+                        hasil < 16.0 -> {
+                            textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Severe Thinnes)"
+                            textResult.setBackgroundResource(R.color.severe_thinnes)
+                        }
+                        hasil in 17.0..16.0 -> {
+                            textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Moderate Thinnes)"
+                            textResult.setBackgroundResource(R.color.moderate_thinnes)
+                        }
+                        hasil in 17.0..19.0 -> {
+                            textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Mild Thinnes)"
+                            textResult.setBackgroundResource(R.color.mild_thinness)
+                        }
+                        hasil in 19.0..25.0 -> {
+                            textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Normal)"
+                            textResult.setBackgroundResource(R.color.normal)
+                        }
+                        hasil in 25.0..30.0 -> {
+                            textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Overweight)"
+                            textResult.setBackgroundResource(R.color.overweight)
+                        }
+                        hasil in 30.0..35.0 -> {
+                            textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Obesity Class 1)"
+                            textResult.setBackgroundResource(R.color.obesity1)
+                        }
+                        hasil in 35.0..40.0 -> {
+                            textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Obesity Class 2)"
+                            textResult.setBackgroundResource(R.color.obesity2)
+                        }
+                        hasil > 40.0 -> {
+                            textResult.text = "BMI = ${"%.2f".format(hasil)} kg/m2   (Obesity Class 3)"
+                            textResult.setBackgroundResource(R.color.obesity3)
+                        }
+                    }
 
-                closeBmi.setOnClickListener {
-                    this.dismiss()
-                }
-            }.show()
+                    closeBmi.setOnClickListener {
+                        this.dismiss()
+                    }
+                }.show()
+            } else {
+                Toast.makeText(this, "Please fill all the form!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/id/kotlin/tugasminggu02/YourAge.kt
+++ app/src/main/java/id/kotlin/tugasminggu02/YourAge.kt
@@ -11,14 +11,26 @@ class YourAge : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_your_age)
 
+
+
         buttonCalculateUmur.setOnClickListener {
-            Toast.makeText(
-                this,
-                "Your age is ${
-                    inputAgeAtDate.text.toString().toInt() - inputBirthdate.text.toString().toInt()
-                } years old",
-                Toast.LENGTH_LONG
-            ).show()
+            val age = inputAgeAtDate.text
+            val birthDate = inputBirthdate.text
+            if (!age.isNullOrEmpty() && !birthDate.isNullOrEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Your age is ${
+                        age.toString().toInt() - birthDate.toString().toInt()
+                    } years old",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else{
+                Toast.makeText(
+                    this,
+                    " Please fill all the Form",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
     }
 }
